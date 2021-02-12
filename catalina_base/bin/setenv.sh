04d84446f6dfce2e0295b524e8c771d69c8c4af5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-02-12 21:28:19 +0100

    this would probably allow for even less noise in Dockerfiles

--- catalina_base/bin/setenv.sh
+++ catalina_base/bin/setenv.sh
@@ -1,1 +1,4 @@
 export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j.configuration=log4j.kibana.xml -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap  -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.rmi.port=3000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost"
+
+export JPDA_ADDRESS=8000
+export JPDA_TRANSPORT=dt_socket
