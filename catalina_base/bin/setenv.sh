1c7b57f347c46a2627ea03ee8d2af060681f8613
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-02-13 14:54:27 +0100

    Made it bit more readable, made the jmx port configurable

--- catalina_base/bin/setenv.sh
+++ catalina_base/bin/setenv.sh
@@ -1,4 +1,18 @@
-export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j.configuration=log4j.kibana.xml -Dlog4j.configurationFile=file://${CATALINA_BASE}/conf/log4j2.kibana.xml -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap  -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.rmi.port=3000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost"
+#! /bin/sh
+
+# log4j 1
+export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j.configuration=log4j.kibana.xml"
+# log4j 2
+export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j.configurationFile=file://${CATALINA_BASE}/conf/log4j2.kibana.xml"
+
+export CATALINA_OPTS="$CATALINA_OPTS -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
+
+if [[ -z "$JMX_PORT" ]]; then
+   JMX_PORT=3000
+fi
+
+# JMX
+export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost"
 
 export JPDA_ADDRESS=8000
 export JPDA_TRANSPORT=dt_socket
