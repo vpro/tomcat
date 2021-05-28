fb7a8c810d7611cd34cdc270e2b968e6ceb334af
Michiel Meeuwissen <git@meeuw.org>
2021-05-28 09:17:02 +0200

    I think this is handy, I'm going to try that out.

--- catalina_base/bin/setenv.sh
+++ catalina_base/bin/setenv.sh
@@ -7,11 +7,20 @@ export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j.configurationFile=file://${CATALINA
 
 export CATALINA_OPTS="$CATALINA_OPTS -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
 
+
+# JMX
 if [[ -z "$JMX_PORT" ]]; then
    JMX_PORT=3000
 fi
+# Since it is hard, if not impossible, to tunnel jmx to different ports on localhost, it is handier if different environment have different port numbers
+if [[ "$POD_NAMESPACE" == *acc ]]; then
+	JMX_PORT=$((JMX_PORT + 100))
+fi
+if [[ "$POD_NAMESPACE" == *test ]]; then
+	JMX_PORT=$((JMX_PORT + 200))
+fi
 
-# JMX
+
 export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost"
 
 # JPDA
