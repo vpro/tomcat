d2ae5633d29c206b05175e6cadef9843e44ca1cc
Michiel Meeuwissen <git@meeuw.org>
2022-09-01 22:31:32 +0200

    Details.

--- catalina_base/bin/start.sh
+++ catalina_base/bin/start.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 export CATALINA_PID=${CATALINA_BASE}/temp/tomcat.pid
-export CATALINA_OUT=${CATALINA_BASE}/logs/catalina.out
+export APPLICATION_OUT=${CATALINA_BASE}/logs/application.out
 trap stop SIGTERM
 
 function start() {
-  touch ${CATALINA_OUT}
-   (catalina.sh $([ "$CATALINA_ARGS" == "" ] && echo "jpda run" || echo "$CATALINA_ARGS") & echo $! > "${CATALINA_PID}") | /usr/bin/rotatelogs -e -L ${CATALINA_OUT} -f ${CATALINA_OUT}.%Y-%m-%d 86400 &
+  # Call catalina.sh which arguments, and pipes output to a file
+  # (analogous to catalina.out)
 
-   tail -F ${CATALINA_OUT} & wait $!
+   (catalina.sh $([ "$CATALINA_ARGS" == "" ] && echo "jpda run" || echo "$CATALINA_ARGS") & echo $! > "${CATALINA_PID}") | /usr/bin/rotatelogs -L ${APPLICATION_OUT} -f  ${APPLICATION_OUT}.%Y-%m-%d 86400 &
+
+   tail -f ${APPLICATION_OUT} & wait $!
 }
 
 stop(){
    catalinaPid=$(cat ${CATALINA_PID})
-   echo "$(date +'%F %T,%3N') killing $catalinaPid" | tee -a "$CATALINA_OUT"
+   echo "$(date +'%F %T,%3N') killing $catalinaPid" | tee -a "${APPLICATION_OUT}"
    kill -SIGTERM $catalinaPid
    exit
 }
