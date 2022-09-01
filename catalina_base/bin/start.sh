25a23eadff398805bf02f098f433a1c059a888dc
Michiel Meeuwissen <git@meeuw.org>
2022-09-01 23:11:44 +0200

    waiting for the process to finish..

--- catalina_base/bin/start.sh
+++ catalina_base/bin/start.sh
@@ -3,6 +3,7 @@
 export CATALINA_PID=${CATALINA_BASE}/temp/tomcat.pid
 export APPLICATION_OUT=${CATALINA_BASE}/logs/application.out
 trap stop SIGTERM
+var tailpid
 
 function start() {
   # Call catalina.sh which arguments, and pipes output to a file
@@ -10,13 +11,18 @@ function start() {
 
    (catalina.sh $([ "$CATALINA_ARGS" == "" ] && echo "jpda run" || echo "$CATALINA_ARGS") & echo $! > "${CATALINA_PID}") | /usr/bin/rotatelogs -L ${APPLICATION_OUT} -f  ${APPLICATION_OUT}.%Y-%m-%d 86400 &
 
-   tail -F ${APPLICATION_OUT} 2>/dev/null & wait $!
+   tail -F ${APPLICATION_OUT} 2>/dev/null & tailpid=$!
+   wait $tailpid
 }
 
 stop(){
    catalinaPid=$(cat ${CATALINA_PID})
    echo "$(date -Iseconds) SIGTERM Killing catalina $catalinaPid" | tee -a "${APPLICATION_OUT}"
    kill -SIGTERM $catalinaPid
+   # Waiting for it to end
+   tail -f /dev/null --pid $catalinaPid
+   # killing tail too
+   kill -9 $tailpid
    exit
 }
 
