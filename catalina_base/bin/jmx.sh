cdb210ccbff5652ce4571b1ffb121c9adca9851a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-09-10 23:34:17 +0200

    The pod name can also be dtermiend witout the charts. It is just the same as hostname.

--- catalina_base/bin/jmx.sh
+++ catalina_base/bin/jmx.sh
@@ -14,6 +14,7 @@ if [[ "$POD_NAMESPACE" == *test ]]; then
   JMX_PORT=$((JMX_PORT + 200))
 fi
 
+POD_NAME=$(hostname)
 POD_NUMBER=${POD_NAME##*-}
 JMX_PORT=$((JMX_PORT + POD_NUMBER))
 echo $JMX_PORT
